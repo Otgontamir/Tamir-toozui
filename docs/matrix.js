3c6b49976e733832768cdced96a3a89f8a78dc2d
Otgontamir <t.otgontamir@gmail.com>
2025-03-30 21:45:56 +0800

    Update matrix.js

--- docs/matrix.js
+++ docs/matrix.js
@@ -1,28 +1,28 @@
-// 1–22 арканы товч тайлбарууд
-const arcanaDescriptions = {
-  1: "Илбэчин: Хүч, санаачилга",
-  2: "Дээд санваартан: Зөн совин, дотоод ухаарал",
-  3: "Хатан хаан: Бүтээлч байдал, халамж",
-  4: "Эзэн хаан: Тогтвортой байдал, манлайлал",
-  5: "Хиерофант: Сүнслэг мэдлэг, уламжлал",
-  6: "Хайрлагчид: Харилцаа, сонголт",
-  7: "Сүйх тэрэг: Зорилго, ялалт",
-  8: "Шударга ёс: Хариуцлага, тэнцвэр",
-  9: "Даяанч: Ганцаардал, гүн ухаан",
-  10: "Азын хүрд: Өөрчлөлт, боломж",
-  11: "Хүч чадал: Хүч, уужуу байдал",
-  12: "Дүүжлэгдсэн: Золиослол, ухаарал",
-  13: "Үхэл: Төгсгөл ба шинэ эхлэл",
-  14: "Зохицуулалт: Эв нэгдэл, эдгэрэл",
-  15: "Чөтгөр: Уруу таталт, хамаарал",
-  16: "Цамхаг: Гэнэтийн нуралт",
-  17: "Од: Итгэл найдвар",
-  18: "Сар: Айдас, хуурмаг байдал",
-  19: "Нар: Аз жаргал, илч",
-  20: "Шүүлт: Гэгээрэл, уучлал",
-  21: "Дэлхий: Амжилт, дуусах шат",
-  22: "Шоглогч: Эхлэл, итгэл"
+const arcanaNames = {
+  1: "Илбэчин — Хүсэл зориг, санаачилга",
+  2: "Дээд санваартан — Зөн совин, мэргэн ухаан",
+  3: "Хатан хаан — Бүтээлч байдал, халамж",
+  4: "Эзэн хаан — Тогтвортой байдал, манлайлал",
+  5: "Хиерофант — Сүнслэг мэдлэг, уламжлал",
+  6: "Хайрлагчид — Сонголт, харилцаа",
+  7: "Сүйх тэрэг — Зорилго, ялалт",
+  8: "Шударга ёс — Үнэнч байдал, хариуцлага",
+  9: "Даяанч — Бясалгал, мэргэн ухаан",
+  10: "Азын хүрд — Өөрчлөлт, боломж",
+  11: "Хүч чадал — Тэвчээр, дотоод хүч",
+  12: "Дүүжлэгдсэн хүн — Золиос, хүлээн зөвшөөрөх",
+  13: "Үхэл — Өөрчлөлт, шинэ эхлэл",
+  14: "Зохицуулалт — Тэнцвэр, дасан зохицол",
+  15: "Чөтгөр — Хамаарал, уруу таталт",
+  16: "Цамхаг — Гэнэтийн өөрчлөлт, нуралт",
+  17: "Од — Итгэл найдвар, урам зориг",
+  18: "Сар — Зөн совин, хуурмаг байдал",
+  19: "Нар — Амжилт, баяр хөөр",
+  20: "Шүүх — Ухамсрын сэргэлт, өөрчлөлт",
+  21: "Дэлхий — Бүтэн байдал, биелэлт",
+  22: "Шоглогч — Эрх чөлөө, адал явдал"
 };
+
 
 // 1–22-д багасгах функц
 function reduceToArcana(num) {
@@ -82,3 +82,40 @@ function generateMatrixReport(birthdate) {
 
   resultDiv.innerHTML = html;
 }
+<script
+function reduceToArcana(num) {
+  while (num > 22) {
+    num = num.toString().split('').reduce((a, b) => a + parseInt(b), 0);
+  }
+  return num === 0 ? 22 : num;
+}
+
+function calculateMatrixPoints(day, month, year) {
+  const arc = {};
+  const dayArc = reduceToArcana(day);
+  const monthArc = reduceToArcana(month);
+  const yearArc = reduceToArcana(year.toString().split('').reduce((a, b) => a + parseInt(b), 0));
+
+  arc.A = reduceToArcana(day + month);
+  arc.B = reduceToArcana(month + yearArc);
+  arc.C = reduceToArcana(arc.A + arc.B);
+  arc.D = reduceToArcana(arc.A + arc.C);
+  arc.G = reduceToArcana(day + yearArc);
+
+  return arc;
+}
+// A–G цэгийн үр дүнг харуулах
+const matrixList = document.getElementById('matrix-list');
+if (matrixList) {
+  matrixList.innerHTML = '';
+  for (let key in arc) {
+    const num = arc[key];
+    const name = arcanaNames[num] || '';
+    matrixList.innerHTML += `<li><b>${key}</b> цэг: Аркан ${num} — ${name}</li>`;
+  }
+}
+const birth = new Date(date);
+const day = birth.getDate();
+const month = birth.getMonth() + 1;
+const year = birth.getFullYear();
+calculateMatrixPoints(day, month, year);
